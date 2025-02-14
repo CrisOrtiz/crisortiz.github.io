99c59155664b77d358f4f34e27a5785a943981d9
Cristhian Ortiz <cristhianortizmercado@gmail.com>
2025-02-14 15:35:28 -0400

    Dev (#24)
    * 2 change node version to fix deployment error (#12)
    
    Co-authored-by: Cristhian Ortiz Mercado <Cristhian.Ortiz@fundacion-jala.org>
    
    * 3 Set up all styling files, fonts and animation (#17)
    
    Co-authored-by: Cristhian Ortiz Mercado <Cristhian.Ortiz@fundacion-jala.org>
    
    * 4 Add translation service (#19)
    
    Co-authored-by: Cristhian Ortiz Mercado <Cristhian.Ortiz@fundacion-jala.org>
    
    * 5 create home and admin components with routing (#23)
    
    * 18 Add translation service
    
    * 5 Create Home and Admin components with routing
    
    ---------
    
    Co-authored-by: Cristhian Ortiz Mercado <Cristhian.Ortiz@fundacion-jala.org>
    
    ---------
    
    Co-authored-by: Cristhian Ortiz Mercado <Cristhian.Ortiz@fundacion-jala.org>

--- angular-project/src/app/app.component.ts
+++ angular-project/src/app/app.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
+import { Title, Meta } from '@angular/platform-browser';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import {LanguageService} from "./services/language.service"
+import { Location, CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, TranslateModule],
+  providers: [LanguageService]
 })
-export class AppComponent {
-  title = 'crisortiz-githubpage';
+
+export class AppComponent implements OnInit {
+  title = 'Cristhian Ortiz |  Developer';
+  
+  constructor(
+    private titleService: Title,
+    private metaService: Meta,
+    private translateService: TranslateService,
+    private location: Location,
+    private languageService: LanguageService
+    ){
+  }
+  
+  ngOnInit(): void{
+    
+    this.languageService.initLanguage();
+
+    this.titleService.setTitle( "Cristhian Ortiz |  Developer" );
+
+    this.metaService.addTags([
+      {name: 'keywords', content: 'Frontend, software, developer'},
+      {name: 'description', content: 'Con 4+ años de experiencia desarrollando software'},
+    ]);
+    
+    AOS.init(); 
+  }
+  
 }
+
+
--- angular-project/src/app/app.config.ts
+++ angular-project/src/app/app.config.ts
@@ -1,8 +1,29 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideServiceWorker } from '@angular/service-worker';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+            provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }),
+          provideHttpClient(), // or provideHttpClient() in Angular v15
+          importProvidersFrom(TranslateModule.forRoot({
+            loader: {
+              provide: TranslateLoader,
+              useFactory: createTranslateLoader,
+              deps: [HttpClient]
+            }
+          })),
+        ]
 };
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
