858242007a26f215e0102ace252d4b22520ba1d3
Cristhian Ortiz Mercado <Cristhian.Ortiz@fundacion-jala.org>
2025-02-14 14:21:23 -0400

    18 Add translation service

--- angular-project/src/app/app.component.ts
+++ angular-project/src/app/app.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
+import { Title, Meta } from '@angular/platform-browser';
+import { TranslateService } from '@ngx-translate/core';
+import {LanguageService} from "./services/language.service"
+import { Location, CommonModule } from '@angular/common';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [CommonModule, RouterModule, RouterOutlet]
 })
-export class AppComponent {
-  title = 'crisortiz-githubpage';
+export class AppComponent implements OnInit {
+  title = 'Cristhian Ortiz |  Developer';
+  
+  constructor(
+    private titleService: Title,
+    private metaService: Meta,
+    private translateService: TranslateService,
+    private location: Location,
+    private languageService: LanguageService
+    ){
+    }
+  ngOnInit(): void{
+    
+    this.languageService.initLanguage();
+
+    this.titleService.setTitle( "Cristhian Ortiz |  Developer" );
+
+    this.metaService.addTags([
+      {name: 'keywords', content: 'Frontend, software, developer'},
+      {name: 'description', content: 'Con 4+ años de experiencia desarrollando software'},
+    ]);
+    
+    AOS.init(); 
+
+  }
 }
