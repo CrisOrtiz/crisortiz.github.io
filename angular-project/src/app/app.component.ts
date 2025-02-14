e58da39ad2b37197ffea4c36c19ca08d6045ad28
Cristhian Ortiz Mercado <Cristhian.Ortiz@fundacion-jala.org>
2025-02-14 15:25:33 -0400

    5 Create Home and Admin components with routing

--- angular-project/src/app/app.component.ts
+++ angular-project/src/app/app.component.ts
@@ -1,18 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import * as AOS from 'aos';
 import { Title, Meta } from '@angular/platform-browser';
-import { TranslateService } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import {LanguageService} from "./services/language.service"
 import { Location, CommonModule } from '@angular/common';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [CommonModule, RouterModule, RouterOutlet]
+  imports: [CommonModule, RouterOutlet, TranslateModule],
+  providers: [LanguageService]
 })
+
 export class AppComponent implements OnInit {
   title = 'Cristhian Ortiz |  Developer';
   
@@ -23,7 +25,8 @@ export class AppComponent implements OnInit {
     private location: Location,
     private languageService: LanguageService
     ){
-    }
+  }
+  
   ngOnInit(): void{
     
     this.languageService.initLanguage();
@@ -36,6 +39,8 @@ export class AppComponent implements OnInit {
     ]);
     
     AOS.init(); 
-
   }
+  
 }
+
+
--- angular-project/src/app/app.routes.ts
+++ angular-project/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'home', component: HomeComponent},
+    {path: 'admin', loadComponent: () => import('./components/admin-panel/admin-panel.component').then((c) => c.AdminPanelComponent)},   
+    {path: '**', pathMatch: 'full', redirectTo: ''},
+];
